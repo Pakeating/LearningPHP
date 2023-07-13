75d4ceb68dbeab4b7f1dce4270e57509c99c7628
Paco <paco.refractando@gmail.com>
2023-07-13 16:17:14 +0200

    revisada la construccion y uso de clases

--- POO/index.php
+++ POO/index.php
@@ -11,7 +11,7 @@
         var $ruedas;
         var $color;
         var $motor;
-        function Coche(){   //constructor
+        function __construct() {   //CONSTRUCTOR DEBE SER ASI, SE CAMBIO EN ALGUN MOMENTO. Antes era como en java.
             $this->ruedas=4;
             $this->color="rojo";
             $this->motor=1600;
@@ -25,6 +25,14 @@
         function frenar(){
             echo "estoy frenando <br>";
         }
+        function establece_color($color_coche){
+            echo "El color de este coche es: ".$this->color."<br>";
+            $this->color=$color_coche;
+            echo "El color de este coche es: ". $this->color."<br>";
+        }
+        function getRuedas(){
+            return $this->ruedas;
+        }
     }
 
     $renault=new Coche();
@@ -32,7 +40,10 @@
     $mazda->arrancar();
     $mazda->girar();
     $renault->frenar();
-    echo $mazda->ruedas;
+    echo "el numero de ruedas es: ". $mazda->ruedas."<br>";
+    echo $mazda->motor."<br>";
+    
+    $mazda->establece_color("Amarillo");
     ?>
 </body>
 </html>
