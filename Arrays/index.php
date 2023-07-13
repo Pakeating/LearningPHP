ae71d3c0aea8eb02ef6f89d321e3e6d24c45383c
Paco <paco.refractando@gmail.com>
2023-07-13 21:24:42 +0200

    acabado arrays

--- Arrays/index.php
+++ Arrays/index.php
@@ -15,6 +15,19 @@
     echo$semana2[1]."<br>";
     $arrayAsociativo=array("Nombre"=>"Juan","Apellido"=>"Gómez","Edad"=>"30");
     echo$arrayAsociativo["Apellido"]."<br>";
+    if (is_array($semana)){
+        echo "es un array <br>";
+    }else {
+        echo "no es un array <br>";
+    }
+    foreach($arrayAsociativo as $clave=>$valor){
+        echo "A $clave le corresponde $valor <br>";
+    }
+    for ($i=0; $i< count ($semana);$i++){
+        echo "A la posicion $i le corresponde el día $semana[$i] <br>";
+    }
+
+    
     ?>
 </body>
-</html>
+</html> 
