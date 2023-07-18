b2a56e87ba938d2791e3f6f04958f029fb046f19
Paco <paco.refractando@gmail.com>
2023-07-18 12:15:47 +0200

    cambiado a db importada

--- DBMySQL/index.php
+++ DBMySQL/index.php
@@ -7,8 +7,9 @@
 </head>
 <body>
     <?php
+    require("ConectionDB.php");
     $db_host="localhost";
-    $db_nombre="concesionario";
+    $db_nombre="pruebas";
     $db_usuario="root";
     $db_contra="francisco";
 
@@ -20,11 +21,11 @@
     mysqli_select_db($conexion, $db_nombre);
     mysqli_set_charset($conexion,"utf8");
 
-    $query="SELECT * FROM PROPIETARIOS";
+    $query="SELECT * FROM ARTÍCULOS";
 
     $resultados=mysqli_query($conexion, $query);    //devuelve un ResultSet
     while($fila=mysqli_fetch_row($resultados)){ //array con el primer resultado del ResultSet. Cada vez que se ejecuta fetch_row cambia al siguiente registro.         
-    echo $fila[0]. ", ".$fila[1].", ".$fila[2]."<br>";
+    echo $fila[0]. ", ".$fila[1].", ".$fila[2]. ", ".$fila[3].", ".$fila[4]."<br>";
     }
     mysqli_close($conexion);
 
