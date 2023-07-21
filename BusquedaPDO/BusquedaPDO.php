9e04a6d851cbc3e0e9015c94d22220e595516ca6
Paco <paco.refractando@gmail.com>
2023-07-21 11:57:42 +0200

    pagina de busqueda con PDO

---
+++ BusquedaPDO/BusquedaPDO.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    /*para hacer consultas preparadas:
+    Utilizando el objeto conexion se prepara con su metodo prepare($sql)
+    Esto devuelve un objeto PDOStatement, que a su vez contiene los metodos execute() y fetch()
+
+    */
+    $busqueda=$_GET["buscar"];
+    try{
+    $base=new PDO("mysql:host=localhost;dbname=pruebas","root","francisco");
+    $base->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    $sql="SELECT NOMBREARTICULO,SECCION,PRECIO, PAISORIGEN FROM ARTÍCULOS WHERE NOMBREARTICULO LIKE ?;";
+    $resultado=$base->prepare($sql);
+    $resultado->execute(array("%".$busqueda."%"));
+    while($registro=$resultado->fetch(PDO::FETCH_ASSOC)){
+        echo"Nombre: ". $registro["NOMBREARTICULO"]." Seccion: ".$registro["SECCION"]." Precio: "
+        . $registro["PRECIO"]." Pais de origen: ".$registro["PAISORIGEN"]."<br>";
+    }
+    $resultado->closeCursor();
+
+    }catch(Exception $e){
+        die("ERROR: ".$e->GetMessage());
+    }
+
+    ?>
+</body>
+</html>
