e12e4d3c070f939ec44737e8cdbf74b21b042088
Paco <paco.refractando@gmail.com>
2023-07-21 11:28:33 +0200

    conexion con PDO

---
+++ PDO/ConPDO.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    try{
+    $base=new PDO("mysql:host=localhost;dbname=puebas","root","francisco");
+    echo"Conexion OK";
+    }catch(Exception $e){
+        die("ERROR: ".$e->GetMessage());
+    }
+
+    ?>
+</body>
+</html>
