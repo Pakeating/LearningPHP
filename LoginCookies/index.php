a1218d81cd49323cc5ea6983de9b375bff08c531
Paco <paco.refractando@gmail.com>
2023-07-24 13:03:02 +0200

    creando login que guarde el usuario en cookies

---
+++ LoginCookies/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    
+
+</head>
+<body>
+<?php
+    if(isset($_POST["enviar"])){
+
+   
+    try{
+        $base= new PDO("mysql:host=localhost;dbname=pruebas","root","francisco");
+        $base->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+        $sql="SELECT * FROM USERS WHERE USER=:login AND PASS=:pass";
+        $resultado=$base->prepare($sql);
+        $login=htmlentities(addslashes($_POST["login"]));
+        $password=htmlentities(addslashes($_POST["password"]));
+        $resultado->bindValue(":login",$login);
+        $resultado->bindValue(":pass", $password);
+        $resultado->execute();
+
+        $registros=$resultado->rowCount();
+        if($registros!=0){
+            session_start();
+            $_SESSION["usuario"]=$_POST["login"];
+           
+        }else{
+           echo"Error: Usuario o contraseña incorrectos";
+        }
+
+
+
+    }catch(Exception $e){
+        die("Error en la conexion con la DB: ". $e->GetMessage());
+    }
+    }
+    ?>
+    <?php
+    if(!isset($_SESSION["usuario"])){
+        include("formulario.html");
+    }else{
+        echo"Hola ". $_SESSION["usuario"];
+        include("contenido.html");
+    }
+    ?>
+
+        
+    
+    
+    
+</body>
+</html>
